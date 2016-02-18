acf3ac4051ee800c5a3c7be424697ab5699bf054
srkneo <yadavmaheshing@gmail.com>
2016-02-18 09:06:30 +0530

    Max

---
+++ ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelValidation.Models;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {        
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            if(string.IsNullOrEmpty(appt.ClientName))
+            {
+                ModelState.AddModelError("ClientName", "Please enter your name");
+            }
+
+            if(ModelState.IsValidField("Date") && DateTime.Now > appt.Date)
+            {
+                ModelState.AddModelError("Date", "Please Enter Date in Future");
+            }
+
+            if(!appt.TermAccepted)
+            {
+                ModelState.AddModelError("TermAccepted", "You must accept the terms");
+            }
+
+            if(ModelState.IsValid)
+            {
+                return View("Completed", appt);
+            }
+            else
+            {
+                return View();
+            }            
+        }
+	}
+}
---
+++ ModelValidation/Models/Appointment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidation.Models
+{
+    public class Appointment
+    {
+        [Required]
+        public string ClientName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage="Please Enter a Date")]
+        public DateTime Date { get; set; }
+
+        [Range(typeof(bool),"true","true",ErrorMessage = "you must accept the terms")]
+        public bool TermAccepted { get; set; }
+    }
+}
